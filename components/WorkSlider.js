de1ccda1713f7ecbc3d9921c0441771b1edd42cc
likkidamu <123432276+likkidamu@users.noreply.github.com>
2025-01-19 18:31:33 -0600

    Update_1 WorkSlider.js

--- components/WorkSlider.js
+++ components/WorkSlider.js
@@ -87,7 +87,7 @@ const WorkSlider = () => {
                       <div className='absolute inset-0 bg-gradient-to-l from-transparent via-[#e838cc] to-[#4a22bd] opacity-0 group-hover:opacity-80 transition-all duration-700'></div>
                       {/* title */}
                       <a 
-                          href="https://example.com" 
+                          href="https://damodhar-ecommerce-fullstack.netlify.app/"
                           target="_blank" 
                           rel="noopener noreferrer" 
                           className="absolute bottom-0 translate-y-full group-hover:-translate-y-10 group-hover:xl:-translate-y-20 transition-all duration-300">
